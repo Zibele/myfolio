fd698ae4ca2bab84d4a45b03d89e5af4aa49bef2
Zibele <Zibele.nqunqa@gmail.com>
2020-12-26 23:12:01 +0200

    header layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
+const colors = require("tailwindcss/colors")
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      transparent: "transparent",
+      current: 'currentColor',
+      dark: "#262E33",
+      turq: "#21E6C1",
+      white: colors.white,
+      black: colors.black,
+    },
     extend: {},
   },
   variants: {
