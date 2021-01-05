8f95fbcc5def6a280a4856590c2f190e606e73a0
Zibele <Zibele.nqunqa@gmail.com>
2021-01-05 22:07:06 +0200

    models update

--- myfolio/my_folio_app/models.py
+++ myfolio/my_folio_app/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.shortcuts import get_object_or_404
 
 class Profile(models.Model):
 
     name = models.CharField(max_length=30)
     surname = models.CharField(max_length=30)
-    jobTitle = models.CharField(max_length=30)
+    job_title = models.CharField(max_length=30)
     
     def __str__(self):
         return self.name + ' ' + self.surname
@@ -14,41 +15,51 @@ class Profile(models.Model):
 
 class Project(models.Model):
 
-    profileId = models.ForeignKey(Profile,on_delete=models.CASCADE)
-    projectTitle = models.CharField(max_length=30)
+    profile_id = models.ForeignKey(Profile,on_delete=models.CASCADE)
+    project_title = models.CharField(max_length=30)
     description = models.TextField()
     
     def __str__(self):
-        return self.projectTitle
+        return self.project_title
     
     class meta:
         db_table='"Projects"'
 
 class Category(models.Model):
 
-    categoryName= models.CharField(max_length=30)
+    category_name= models.CharField(max_length=30)
     
     def __str__(self):
-        return self.categoryName 
+        return self.category_name 
     
     class meta:
         db_table='"Categories"'
 
+
+
 class Skill(models.Model):
 
-    categoryId=models.ForeignKey(Category, on_delete=models.CASCADE)
-    skillName= models.CharField(max_length=30)
+    category_id=models.ForeignKey(Category, on_delete=models.CASCADE)
+    skill_name= models.CharField(max_length=30)
     
     def __str__(self):
-        return self.skillName
+        return self.skill_name
 
     class meta:
         db_table='"Skills"'
 
+class ProfileSkill(models.Model):
+
+    profile_id = models.ForeignKey(Profile,on_delete = models.CASCADE)
+    skill_id=models.ForeignKey(Skill,on_delete= models.CASCADE)
+
+    class meta:
+        db_table='"ProfileSkills"'
+
 class TechStack(models.Model):
 
-    projectId = models.ForeignKey(Project, on_delete=models.CASCADE)
-    skillId = models.ForeignKey(Skill, on_delete=models.CASCADE)
+    project_id = models.ForeignKey(Project, on_delete=models.CASCADE)
+    skill_id = models.ForeignKey(Skill, on_delete=models.CASCADE)
 
     class meta:
         db_table='"TechStack"'
--- myfolio/my_folio_app/views.py
+++ myfolio/my_folio_app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Profile, ProfileSkill, Project, TechStack,Skill
 
-# Create your views here.
+
+
+def index_page(request):
+
+    profile = Profile.objects.get(name='Zibele')
+    projects = Project.objects.filter(profile_id=profile)
+    techStack = TechStack.objects.all()
+    skills = ProfileSkill.objects.filter(profile_id=profile)
+
+    return render(request,'myfolio/my_folio_app/templates/myfolio/my_folio_app/index.html',
+    {'profile' : Profile, })
