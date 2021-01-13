03fa2b50099e1b4a79407ad017568b01b8e4b5aa
Zibele <Zibele.nqunqa@gmail.com>
2021-01-13 23:20:10 +0200

    Dynamic skills section

--- myfolio/my_folio_app/static/dist/main.js
+++ myfolio/my_folio_app/static/dist/main.js
@@ -1,18 +1,30 @@
 
 
 let skillBtnGroup = document.querySelectorAll('#skillBtn');
+console.log(skillBtnGroup.length);
 
 skillBtnGroup.forEach( (btn) => {
 
     btn.addEventListener( "click", () => {
+            toggleSkills(btn.value);
+        });
 
-        
-        
+    });
 
-    })
 
-}
-);
+function toggleSkills(category){
+    
+    let categories = document.querySelectorAll('#skills > div')
+    
+    categories.forEach( (c) => {
+
+        c.id == category ? c.className ="skillbox-flex" : c.className = "skillbox-hidden";
+     
+    }); 
+    
+
+};
+
 
 
 
